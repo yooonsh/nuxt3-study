33c7c6ca4b8a156f900c113b11d03025106c90af
yoonsh <yoon_sh@cowave.kr>
2023-05-15 14:21:25 +0900

    'first'

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<!-- reactive -->
+<template>
+  <div>{{ state.count }}</div>
+  <div>{{ state.doubled }}</div>
+</template>
+
+<script setup>
+const state = reactive({
+  count: 1,
+  doubled: computed(() => state.count * 2),
+});
+
+const set = new Set(["apple", "banana", "orange"]);
+
+// set 값 순회
+set.forEach((val, val2, set) => {
+  console.log(val, val2, set);
+});
+
+console.log(state.count); // 1
+</script>
