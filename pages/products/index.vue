fd44535fa55cb5cd80d1368cd40c218b0484d999
sunghee <sunghe1016@naver.com>
2023-05-25 17:40:41 +0900

    write 링크수정

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -11,15 +11,15 @@
         </p>
       </div>
       <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <button
-          type="button"
+        <a
+          href="/products/write"
           class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Add product
-        </button>
+        </a>
       </div>
     </div>
-    <div class="mt-8 flow-root">
+    <div class="mt-8 mb-8 flow-root border-b border-gray-200">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
           <table class="min-w-full divide-y divide-gray-300">
@@ -85,10 +85,88 @@
         </div>
       </div>
     </div>
+    <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+      <div>
+        <p class="text-sm text-gray-700">
+          Showing
+          {{ " " }}
+          <span class="font-medium">1</span>
+          {{ " " }}
+          to
+          {{ " " }}
+          <span class="font-medium">10</span>
+          {{ " " }}
+          of
+          {{ " " }}
+          <span class="font-medium">97</span>
+          {{ " " }}
+          results
+        </p>
+      </div>
+      <div>
+        <nav
+          class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+          aria-label="Pagination"
+        >
+          <a
+            href="#"
+            class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          >
+            <span class="sr-only">Previous</span>
+            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+          </a>
+          <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
+          <a
+            href="#"
+            aria-current="page"
+            class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >1</a
+          >
+          <a
+            href="#"
+            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >2</a
+          >
+          <a
+            href="#"
+            class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+            >3</a
+          >
+          <span
+            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+            >...</span
+          >
+          <a
+            href="#"
+            class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+            >8</a
+          >
+          <a
+            href="#"
+            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >9</a
+          >
+          <a
+            href="#"
+            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >10</a
+          >
+          <a
+            href="#"
+            class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+          >
+            <span class="sr-only">Next</span>
+            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+          </a>
+        </nav>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/20/solid";
+
 const { $api }: any = useNuxtApp();
 const data = ref();
 data.value = await $api.getProduct();
